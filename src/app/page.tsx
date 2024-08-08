708570ef2ff1c845b3e294a48f4d726a708b4310
vincent <mcleesevj@gmail.com>
2024-08-08 19:38:09 +0200

    removed global.css styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>New app!</h1>
+      <h1>This is the homepage!</h1>
     </main>
   );
 }
