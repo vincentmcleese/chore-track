a9e10674c1876d07dd94115d783f86ccc314d4f0
vincent <mcleesevj@gmail.com>
2024-08-08 19:53:33 +0200

    installed and tested nextui global

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { Button } from "@nextui-org/react";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between px-24">
       <h1>This is the homepage!</h1>
+      <Button>Hello</Button>
     </main>
   );
 }
