17a44e03e399fe3cd2d8d62e84a0692704076ac4
vincent <mcleesevj@gmail.com>
2024-08-14 13:14:01 +0200

    added buttons to navbar and added the svg icon(s)

--- src/components/common/navbar.tsx
+++ src/components/common/navbar.tsx
@@ -6,18 +6,40 @@ import {
   NavbarBrand,
   NavbarItem,
   NavbarMenuItem,
+  Button,
 } from "@nextui-org/react";
+import { Link } from "@nextui-org/link";
+import { link as linkStyles } from "@nextui-org/theme";
+import NextLink from "next/link";
+import { Logo } from "@/components/common/icons";
 
 export const Navbar = () => {
   return (
     <NextUINavbar maxWidth="xl" position="sticky">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand as="li" className="gap-3 max-w-fit">
-          Logo
+          <Logo />
+          <NextLink className="flex justify-start items-center gap-1" href="/">
+            Nigellestraat12
+          </NextLink>
         </NavbarBrand>
-
         <NavbarItem>
-          <p>item</p>
+          <NextLink className="" color="foreground" href="/manage">
+            manage
+          </NextLink>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent
+        className="hidden sm:flex basis-1/5 sm:basis-full"
+        justify="end"
+      >
+        <NavbarItem>
+          <form action={" "}>
+            <Button type="submit" color="secondary" variant="bordered">
+              {" "}
+              Sign In
+            </Button>
+          </form>
         </NavbarItem>
       </NavbarContent>
     </NextUINavbar>
