d44a61816a175939dd4fd5718e04b4853f450710
vincent <mcleesevj@gmail.com>
2024-08-14 13:26:53 +0200

    added the auth.ts file and added comments on how to set it up

---
+++ src/auth.ts
@@ -0,0 +1,36 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { db } from "@/src/db";
+
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+
+if (!GOOGLE_CLIENT_ID || !GOOGLE_CLIENT_SECRET) {
+  throw new Error("GOOGLE_CLIENT_ID and GOOGLE_CLIENT_SECRET must be set");
+}
+
+export const {
+  handlers: { GET, POST },
+  auth,
+  signOut,
+  signIn,
+} = NextAuth({
+  adapter: PrismaAdapter(db),
+  providers: [
+    GoogleProvider({
+      clientId: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    //ususally not needed, fixing a bug in nextauth
+    async session({ session, user }: any) {
+      if (session && user) {
+        session.user.id = user.id;
+      }
+
+      return session;
+    },
+  },
+});
