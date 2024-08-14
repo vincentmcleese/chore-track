c29b7b3c064566deceb4dbf3e2b56d152add7988
vincent <mcleesevj@gmail.com>
2024-08-14 13:24:44 +0200

    added the actions structure from previous app (index/createchore/createcompletion/signin and out

---
+++ src/actions/sign-in.ts
@@ -0,0 +1,7 @@
+"use server";
+
+import * as auth from "@/src/auth";
+
+export async function signIn() {
+  return auth.signIn("google");
+}
---
+++ src/actions/sign-out.ts
@@ -0,0 +1,7 @@
+"use server";
+
+import * as auth from "@/src/auth";
+
+export async function signOut() {
+  return auth.signOut();
+}
