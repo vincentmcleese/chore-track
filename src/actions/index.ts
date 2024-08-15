c4d603c38f6a6ed6451d2d658f1485a8d2256932
vincent <mcleesevj@gmail.com>
2024-08-15 22:31:58 +0200

    commeted out create compeltion from index

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,4 +1,4 @@
 export { signIn } from "./sign-in";
 export { signOut } from "./sign-out";
 // export { createChore } from "./create-chore";
-export { createCompletion } from "./create-completion";
+// export { createCompletion } from "./create-completion";
