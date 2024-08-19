36e4fc638aeea0a7718edb013691928b3dea510a
vincent <mcleesevj@gmail.com>
2024-08-19 14:26:54 +0200

    testing a db call from the cronjob

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,10 +1,18 @@
 import { EmailTemplate } from "@/components/email/template";
 import { Resend } from "resend";
 import * as React from "react";
+import { db } from "@/db";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function GET(request: Request) {
+  // Get the title of max one chore in the database
+  const chore = await db.chore.findFirst({
+    select: {
+      title: true,
+    },
+  });
+
   try {
     console.log("Received request:", {
       method: request.method,
@@ -15,7 +23,7 @@ export async function GET(request: Request) {
     const { data, error } = await resend.emails.send({
       from: "Nimbus <nimbus@nigellestraat12.com>",
       to: "mcleesevj@gmail.com",
-      subject: "Cronjob",
+      subject: `Cronjob for ${chore?.title}`,
       react: EmailTemplate({ firstName: "Vincent" }) as React.ReactElement,
     });
 
