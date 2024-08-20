ed0882ca132f0d67621d492534987e6841f77a10
vincent <mcleesevj@gmail.com>
2024-08-20 20:54:19 +0200

    updated completion email with working image

--- src/components/email/completion-template.tsx
+++ src/components/email/completion-template.tsx
@@ -14,11 +14,15 @@ export const CompletionTemplate: React.FC<
       {title} is marked complete. It was completed with a status of: {status}.
       Love, Nimbus.
     </p>
+    <p style={{ fontWeight: "bold", fontSize: "1.2em", marginTop: "20px" }}>
+      Love, Nimbus.
+    </p>
     <Img
-      src="http://drive.google.com/uc?export=view&id=1adbooetKdiDc2lSpv2rIeRJAo-mcnjv4"
+      src="http://drive.google.com/uc?export=view&id=1guy7YENhLXB1O72tDdQlDuQtPEmwItIS"
       alt="nimbus"
       width="300"
-      height="500"
+      height="300"
+      style={{ borderRadius: "50%" }}
     />
     ;
   </div>
