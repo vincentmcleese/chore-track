ce705dbe8271f97c98e12a877b02670df8e81c6b
vincent <mcleesevj@gmail.com>
2024-08-25 17:28:28 +0200

    reminder email

---
+++ src/app/api/cron/reminder/route.ts
@@ -0,0 +1,82 @@
+import {
+  calculateDaysSinceCompletion,
+  checkIfOverdue,
+  sortChores,
+} from "@/services/choreUtils";
+import { WeeklyTemplate } from "@/components/email/weekly-template";
+import { Resend } from "resend";
+import * as React from "react";
+import { db } from "@/db";
+import { ReminderTemplate } from "@/components/email/reminder-template";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function GET(request: Request) {
+  //chores
+  const chores = await db.chore.findMany({
+    include: {
+      completions: {
+        orderBy: { completedAt: "desc" },
+        take: 1,
+      },
+      assignee: {
+        select: {
+          name: true,
+          image: true,
+          email: true,
+        },
+      },
+    },
+  });
+
+  // Add "days since last completion" to each chore
+  chores.forEach((chore: any) => {
+    chore.daysSinceCompletion = calculateDaysSinceCompletion(chore.completions);
+  });
+
+  // Check if days since last completion is greater than interval and add a status attribute
+  chores.forEach((chore: any) => {
+    chore.status = checkIfOverdue(chore.daysSinceCompletion, chore.interval);
+  });
+
+  // add a next due date to each chore
+  chores.forEach((chore: any) => {
+    chore.nextDueDate = new Date(
+      chore.completions[0].completedAt.getTime() +
+        chore.interval * 7 * 24 * 60 * 60 * 1000
+    );
+  });
+
+  //filter chores with assignee email equal to mcleesevj@gmail.com
+  const userChores = chores.filter(
+    (chore) => chore.assignee && chore.assignee.email === "mcleesevj@gmail.com"
+  );
+
+  // Sort chores with overdue first (sorted by most overdue), then current (sorted by least overdue)
+  const sortedChores = sortChores(userChores);
+
+  try {
+    console.log("Received request:", {
+      method: request.method,
+      url: request.url,
+    });
+
+    // Send the email with updated chores
+    const { data, error } = await resend.emails.send({
+      from: "Dr. Stoffels <stoffels@nigellestraat12.com>",
+      to: "mcleesevj@gmail.com",
+      subject: `Your upcoming chores`,
+      react: ReminderTemplate({ chores: sortedChores }) as React.ReactElement,
+    });
+
+    if (error) {
+      console.error("Error sending email:", error);
+      return Response.json({ error }, { status: 500 });
+    }
+
+    return Response.json({ data });
+  } catch (error) {
+    console.error("Error in GET request:", error);
+    return Response.json({ error }, { status: 500 });
+  }
+}
---
+++ src/components/email/reminder-template.tsx
@@ -0,0 +1,143 @@
+import {
+  Body,
+  Button,
+  Container,
+  Column,
+  Head,
+  Heading,
+  Hr,
+  Html,
+  Img,
+  Link,
+  Preview,
+  Row,
+  Section,
+  Text,
+  Tailwind,
+} from "@react-email/components";
+
+// Define the interface for a single chore
+// Define the interface for a single chore
+interface Chore {
+  id: string;
+  title: string;
+  daysSinceCompletion: number | null;
+  nextDue: Date;
+  status: string;
+  assignee: {
+    name: string;
+    image: string;
+  };
+  completions: {
+    completedAt: Date;
+  }[];
+}
+
+// Define the interface for the props of the WeeklyTemplate component
+interface ReminderTemplateProps {
+  chores: Chore[];
+}
+
+export const ReminderTemplate: React.FC<Readonly<ReminderTemplateProps>> = ({
+  chores,
+}) => (
+  <Html>
+    <Head />
+    <Preview>Your upcoming chores</Preview>
+    <Tailwind>
+      <Body className="bg-white my-auto mx-auto font-sans px-2">
+        <Container className="border border-solid border-[#eaeaea] rounded my-[40px] mx-auto p-[20px] max-w-[465px]">
+          <Section className="mt-[32px]">
+            <Img
+              src="http://drive.google.com/uc?export=view&id=1YdxGYSo3yLG4nClW0BjP1jrQ1iXn1XBx"
+              width="64"
+              height="64"
+              alt="Vercel"
+              className="my-0 mx-auto rounded-full"
+            />
+          </Section>
+          <Heading className="text-black text-[24px] font-normal text-center p-0 my-[30px] mx-0">
+            The Dr. Stoffel&#39;s Weekly Roundup
+          </Heading>
+          <Text className="text-black text-[14px] leading-[24px]">
+            Here is the status of chores this week:
+          </Text>
+          <Section>
+            {chores.map((chore) => (
+              <Row key={chore.id}>
+                <Column align="left">
+                  <Text className="text-black text-[14px] leading-[24px]">
+                    {chore.title}
+                  </Text>
+                </Column>
+                <Column align="center">
+                  <Text
+                    className={`${
+                      chore.status === "overdue"
+                        ? "text-red-600"
+                        : "text-green-600"
+                    } text-[14px] leading-[24px]`}
+                  >
+                    {chore.status}
+                  </Text>
+                </Column>
+                <Column align="right">
+                  <Text className="text-black text-[14px] leading-[24px]">
+                    {chore.assignee?.name?.split(" ")[0]}
+                  </Text>
+                </Column>
+              </Row>
+            ))}
+          </Section>
+          <Section className="text-center mt-[32px] mb-[32px]">
+            <Button
+              className="bg-[#000000] rounded text-white text-[12px] font-semibold no-underline text-center px-5 py-3"
+              href="https://nigellestraat12.com/"
+            >
+              Go to the app
+            </Button>
+          </Section>
+          <Text className="text-black text-[14px] leading-[24px]">
+            or copy and paste this URL into your browser:{" "}
+            <Link
+              href="https://nigellestraat12.com/"
+              className="text-blue-600 no-underline"
+            >
+              https://nigellestraat12.com/
+            </Link>
+          </Text>
+          <Hr className="border border-solid border-[#eaeaea] my-[26px] mx-0 w-full" />
+          <Text className="text-[#666666] text-[12px] leading-[24px]">
+            This weekly roundup is sent every monday morning. If you would like
+            to unsubscribe, too bad. You&#39;re stuck with me.
+          </Text>
+        </Container>
+      </Body>
+    </Tailwind>
+  </Html>
+);
+
+{
+  /* <div>
+      <h1>Here is the weekly roundup!</h1>
+      <p>Here is the week in review:</p>
+      {chores.map((chore) => (
+        <div key={chore.id}>
+          <p>Chore: {chore.title}</p>
+          <p>Status: {chore.status}</p>
+          <p>Completed at: {chore.completions[0].completedAt.toDateString()}</p>
+          <p>Owner: {chore.assignee.name}</p>
+        </div>
+      ))}
+      <Img
+        src="http://drive.google.com/uc?export=view&id=1YdxGYSo3yLG4nClW0BjP1jrQ1iXn1XBx"
+        alt="nimbus"
+        width="100"
+        height="100"
+        style={{ borderRadius: "50%" }}
+      />
+      <p style={{ fontWeight: "bold", fontSize: "1.2em", marginTop: "20px" }}>
+        Love, Dr. Stoffels.
+      </p>
+    </div> */
+}
