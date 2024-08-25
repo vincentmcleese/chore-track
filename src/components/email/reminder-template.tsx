cd27ead767b2299b324d8b253045cbd10f7a9379
vincent <mcleesevj@gmail.com>
2024-08-25 19:00:00 +0200

    next due possibly null v6

--- src/components/email/reminder-template.tsx
+++ src/components/email/reminder-template.tsx
@@ -42,104 +42,105 @@ interface ReminderTemplateProps {
 export const ReminderTemplate: React.FC<Readonly<ReminderTemplateProps>> = ({
   chores,
   name,
-}) => (
-  <Html>
-    <Head />
-    <Preview>{name}, Your upcoming chores</Preview>
-    <Tailwind>
-      <Body className="bg-white my-auto mx-auto font-sans px-2">
-        <Container className="border border-solid border-[#eaeaea] rounded my-[40px] mx-auto p-[20px] max-w-[465px]">
-          <Section className="mt-[32px]">
-            <Img
-              src="http://drive.google.com/uc?export=view&id=1YdxGYSo3yLG4nClW0BjP1jrQ1iXn1XBx"
-              width="64"
-              height="64"
-              alt="Vercel"
-              className="my-0 mx-auto rounded-full"
-            />
-          </Section>
-          <Heading className="text-black text-[24px] font-normal text-center p-0 my-[30px] mx-0">
-            The Dr. Stoffel&#39;s Pre-weekend reminder
-          </Heading>
-          <Text className="text-black text-[14px] leading-[24px]">
-            Happy Friday! While i&#39;m munching on some andijvie, it looks like
-            you&#39;re going to be busy because you have X chores to do this
-            weeked.
-          </Text>
-          <Section>
-            {chores.map((chore) => (
-              <Row key={chore.id}>
-                <Column align="left">
-                  <Text className="text-black text-[14px] leading-[24px]">
-                    {chore.title}
-                  </Text>
-                </Column>
-                <Column align="center">
-                  <Text
-                    className={`${
-                      chore.status === "overdue"
-                        ? "text-red-600"
-                        : "text-green-600"
-                    } text-[14px] leading-[24px]`}
-                  >
-                    {chore.status}
-                  </Text>
-                </Column>
-                <Column align="right">
-                  <Text>{chore.nextDue ?? "n.a"}</Text>
-                </Column>
-              </Row>
-            ))}
-          </Section>
-          <Section className="text-center mt-[32px] mb-[32px]">
-            <Button
-              className="bg-[#000000] rounded text-white text-[12px] font-semibold no-underline text-center px-5 py-3"
-              href="https://nigellestraat12.com/"
-            >
-              Go to the app
-            </Button>
-          </Section>
-          <Text className="text-black text-[14px] leading-[24px]">
-            or copy and paste this URL into your browser:{" "}
-            <Link
-              href="https://nigellestraat12.com/"
-              className="text-blue-600 no-underline"
-            >
-              https://nigellestraat12.com/
-            </Link>
-          </Text>
-          <Hr className="border border-solid border-[#eaeaea] my-[26px] mx-0 w-full" />
-          <Text className="text-[#666666] text-[12px] leading-[24px]">
-            This weekly roundup is sent every monday morning. If you would like
-            to unsubscribe, too bad. You&#39;re stuck with me.
-          </Text>
-        </Container>
-      </Body>
-    </Tailwind>
-  </Html>
-);
+}) => {
+  //filter chores array for chores with status of overdue
+  const overdueChores = chores.filter((chore) => chore.status === "overdue");
 
-{
-  /* <div>
-      <h1>Here is the weekly roundup!</h1>
-      <p>Here is the week in review:</p>
-      {chores.map((chore) => (
-        <div key={chore.id}>
-          <p>Chore: {chore.title}</p>
-          <p>Status: {chore.status}</p>
-          <p>Completed at: {chore.completions[0].completedAt.toDateString()}</p>
-          <p>Owner: {chore.assignee.name}</p>
-        </div>
-      ))}
-      <Img
-        src="http://drive.google.com/uc?export=view&id=1YdxGYSo3yLG4nClW0BjP1jrQ1iXn1XBx"
-        alt="nimbus"
-        width="100"
-        height="100"
-        style={{ borderRadius: "50%" }}
-      />
-      <p style={{ fontWeight: "bold", fontSize: "1.2em", marginTop: "20px" }}>
-        Love, Dr. Stoffels.
-      </p>
-    </div> */
-}
+  //filter chores array for chores with nextdue less than 8
+  const dueSoonChores = chores.filter(
+    (chore) => chore.nextDue && chore.nextDue < 8
+  );
+
+  return (
+    <Html>
+      <Head />
+      <Preview>{name}, Your upcoming chores</Preview>
+      <Tailwind>
+        <Body className="bg-white my-auto mx-auto font-sans px-2">
+          <Container className="border border-solid border-[#eaeaea] rounded my-[40px] mx-auto p-[20px] max-w-[465px]">
+            <Section className="mt-[32px]">
+              <Img
+                src="http://drive.google.com/uc?export=view&id=1YdxGYSo3yLG4nClW0BjP1jrQ1iXn1XBx"
+                width="64"
+                height="64"
+                alt="Vercel"
+                className="my-0 mx-auto rounded-full"
+              />
+            </Section>
+            <Heading className="text-black text-[24px] font-normal text-center p-0 my-[30px] mx-0">
+              The Dr. Stoffel&#39;s Pre-weekend reminder
+            </Heading>
+            <Text className="text-black text-[14px] leading-[24px]">
+              Happy Friday! While i&#39;m munching on some andijvie, it looks
+              like you&#39;re going to be busy because you have{" "}
+              {dueSoonChores.length} chores to do in the next 7 days.
+            </Text>
+
+            {overdueChores.length > 0 && (
+              <Section>
+                <Heading className="text-red-600 text-[20px] font-normal text-center p-0 my-[20px] mx-0">
+                  Overdue Chores
+                </Heading>
+                {overdueChores.map((chore) => (
+                  <Row key={chore.id}>
+                    <Column align="left">
+                      <Text className="text-black text-[14px] leading-[24px]">
+                        {chore.title}
+                      </Text>
+                    </Column>
+                    <Column align="center">
+                      <Text className="text-red-600 text-[14px] leading-[24px]">
+                        Overdue
+                      </Text>
+                    </Column>
+                  </Row>
+                ))}
+              </Section>
+            )}
+
+            <Section>
+              <Heading className="text-red-600 text-[20px] font-normal text-center p-0 my-[20px] mx-0">
+                Upcoming Chores
+              </Heading>
+              {dueSoonChores.map((chore) => (
+                <Row key={chore.id}>
+                  <Column align="left">
+                    <Text className="text-black text-[14px] leading-[24px]">
+                      {chore.title}
+                    </Text>
+                  </Column>
+
+                  <Column align="right">
+                    <Text>{chore.nextDue ?? "n.a"} day&#39;9s)</Text>
+                  </Column>
+                </Row>
+              ))}
+            </Section>
+            <Section className="text-center mt-[32px] mb-[32px]">
+              <Button
+                className="bg-[#000000] rounded text-white text-[12px] font-semibold no-underline text-center px-5 py-3"
+                href="https://nigellestraat12.com/"
+              >
+                Go to the app
+              </Button>
+            </Section>
+            <Text className="text-black text-[14px] leading-[24px]">
+              or copy and paste this URL into your browser:{" "}
+              <Link
+                href="https://nigellestraat12.com/"
+                className="text-blue-600 no-underline"
+              >
+                https://nigellestraat12.com/
+              </Link>
+            </Text>
+            <Hr className="border border-solid border-[#eaeaea] my-[26px] mx-0 w-full" />
+            <Text className="text-[#666666] text-[12px] leading-[24px]">
+              This weekly roundup is sent every monday morning. If you would
+              like to unsubscribe, too bad. You&#39;re stuck with me.
+            </Text>
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
+};
