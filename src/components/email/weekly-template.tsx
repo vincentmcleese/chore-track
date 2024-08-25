6306a32eb618877bf89f1e0f80430691cd2b205b
vincent <mcleesevj@gmail.com>
2024-08-25 09:28:49 +0200

    react html email test

--- src/components/email/weekly-template.tsx
+++ src/components/email/weekly-template.tsx
@@ -1,4 +1,5 @@
 import { Img } from "@react-email/components";
+import { Button, Html } from "@react-email/components";
 
 // Define the interface for a single chore
 interface Chore {
@@ -23,26 +24,34 @@ interface WeeklyTemplateProps {
 export const WeeklyTemplate: React.FC<Readonly<WeeklyTemplateProps>> = ({
   chores,
 }) => (
-  <div>
-    <h1>Here is the weekly roundup!</h1>
-    <p>Here is the week in review:</p>
-    {chores.map((chore) => (
-      <div key={chore.id}>
-        <p>Chore: {chore.title}</p>
-        <p>Status: {chore.status}</p>
-        <p>Completed at: {chore.completions[0].completedAt.toDateString()}</p>
-        <p>Owner: {chore.assignee.name}</p>
-      </div>
-    ))}
-    <Img
-      src="http://drive.google.com/uc?export=view&id=1YdxGYSo3yLG4nClW0BjP1jrQ1iXn1XBx"
-      alt="nimbus"
-      width="100"
-      height="100"
-      style={{ borderRadius: "50%" }}
-    />
-    <p style={{ fontWeight: "bold", fontSize: "1.2em", marginTop: "20px" }}>
-      Love, Dr. Stoffels.
-    </p>
-  </div>
+  <Html>
+    <Button
+      href="https://example.com"
+      style={{ background: "#000", color: "#fff", padding: "12px 20px" }}
+    >
+      Click me
+    </Button>
+    {/* <div>
+      <h1>Here is the weekly roundup!</h1>
+      <p>Here is the week in review:</p>
+      {chores.map((chore) => (
+        <div key={chore.id}>
+          <p>Chore: {chore.title}</p>
+          <p>Status: {chore.status}</p>
+          <p>Completed at: {chore.completions[0].completedAt.toDateString()}</p>
+          <p>Owner: {chore.assignee.name}</p>
+        </div>
+      ))}
+      <Img
+        src="http://drive.google.com/uc?export=view&id=1YdxGYSo3yLG4nClW0BjP1jrQ1iXn1XBx"
+        alt="nimbus"
+        width="100"
+        height="100"
+        style={{ borderRadius: "50%" }}
+      />
+      <p style={{ fontWeight: "bold", fontSize: "1.2em", marginTop: "20px" }}>
+        Love, Dr. Stoffels.
+      </p>
+    </div> */}
+  </Html>
 );
